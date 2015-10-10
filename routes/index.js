c0b51629bab5252d5ae62e3afe2c2dc7d3d5b74a
Armand Niampa <armand.niampa@gmail.com>
2015-10-10 19:55:10 +0200

    Implement Ace.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  kclass = { html: 'session', body: 'valign-wrapper center-align' }
-  res.render('index', { title: '8ditor | Connexion', kclass: kclass });
+router.route('/')
+  /* GET home page. */
+  .get(function(req, res, next) {
+    kclass = { html: 'session', body: 'bi valign-wrapper center-align' }
+    res.render('index', { title: '8ditor | Connexion', kclass: kclass });
+  })
+
+  .post(function(req, res, next) {
+    res.redirect('/editor');
+  });
+
+/* GET editor page. */
+router.get('/editor', function(req, res, next) {
+  kclass = null;
+  res.render('editor', { title: '8ditor | Editor' });
 });
 
 module.exports = router;
