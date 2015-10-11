757bf1bc0c105967c72ba2d0e9c660cd220359f5
Armand Niampa <armand.niampa@gmail.com>
2015-10-12 00:15:02 +0200

    Restrict route access.

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,23 @@ var session;
 router.route('/')
   /* GET home page. */
   .get(function(req, res, next) {
-    session = req.session;
-    kclass = { html: 'session', body: 'bi valign-wrapper center-align' }
-    res.render('home/index', { title: '8ditor | Connexion', kclass: kclass });
+    if (req.session.pseudo) {
+      res.redirect('/editor');
+    }else{
+      kclass = { html: 'session', body: 'bi valign-wrapper center-align' }
+      res.render('home/index', { title: '8ditor | Connexion', kclass: kclass });
+    }
   })
 
   .post(function(req, res, next) {
     var pseudo = req.body.pseudo;
-    session = req.session;
-    session.pseudo = pseudo;
-    res.redirect('/editor');
+    if (pseudo.length > 0) {
+      req.session.pseudo = pseudo;
+      res.redirect('/editor');
+    } else {
+      req.flash('error', 'Merci de renseigner votre Pseudo!');
+      res.redirect('/');
+    }
   });
 
 /* GET editor page. */
